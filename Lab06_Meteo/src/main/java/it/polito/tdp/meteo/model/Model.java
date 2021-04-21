6c01c3a910045eb6c151a258d1a2a0127a07565a
enric <enric@PC-Enrico.homenet.telecomitalia.it>
2021-04-21 11:24:48 +0200

    provvisorio

--- Lab06_Meteo/src/main/java/it/polito/tdp/meteo/model/Model.java
+++ Lab06_Meteo/src/main/java/it/polito/tdp/meteo/model/Model.java
@@ -3,6 +3,7 @@ package it.polito.tdp.meteo.model;
 import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -21,9 +22,8 @@ public class Model {
 	List<List<Rilevamento>> rilevamenti;
 	List<Citta> sequenza;
 	Map<Citta, List<Rilevamento>> mappa;
-	int ricorsioni = 0;
-	int livRic = 0;
-	int costo = 0;
+	int cambi = 0;
+	
 
 	public Model() {
 		mDao = new MeteoDAO();
@@ -53,63 +53,32 @@ public class Model {
 	
 	// of course you can change the String output with what you think works best
 	public List<Citta> trovaSequenza(int mese) {
-		sequenza = new ArrayList<Citta>();
-		List<Citta> parziale = new ArrayList<Citta>();
-		cerca(parziale, 0, mese);
+		sequenza = new LinkedList<Citta>();
+		LinkedList<Citta> parzialeC = new LinkedList<Citta>();
+		Map<List<Citta>, Integer> parziale = new HashMap<List<Citta>, Integer>();
+		cerca(parziale, 0, mese, parzialeC);
 		return sequenza;
 	}
 	
-	private void cerca(List<Citta> sequenzaParziale, int livello, int mese) {
-		ricorsioni ++;
-	
-		if(sequenza.size() == NUMERO_GIORNI_TOTALI) {
+	private void cerca(Map<List<Citta>, Integer> mappaParziale, int livello, int mese, LinkedList<Citta> cittaParziale) {
+		
+		if(cittaParziale.size() == NUMERO_GIORNI_TOTALI) {
 			//caso terminale
 			return;
 		}
 		else {
-			int sommaUmidita = 0;
-			int umidita = 0;
-			for(livRic = livello; livRic < ricorsioni*NUMERO_GIORNI_CITTA_CONSECUTIVI_MIN; livRic++) {
-				for(Citta c : rilevamentiCitta(mese).keySet()) {
-					List<Rilevamento> r = rilevamentiCitta(mese).get(c);
-					if(umidita == 0) {
-						umidita = r.get(livello).getUmidita();
-						c.setSomma(umidita);
-					}
-					else {
-						if(umidita > r.get(livello).getUmidita()) {
-							umidita = r.get(livello).getUmidita();
-							c.setSomma(umidita);
-						}
-					}
-				}
-			}
-			
-				
-			if(livello == ricorsioni*NUMERO_GIORNI_CITTA_CONSECUTIVI_MIN) {
-				for(Citta i : mDao.getCitta()) {
-					if(sommaUmidita == 0) {
-						sommaUmidita = i.getSomma();
-						sequenzaParziale.add(i);
-						sequenzaParziale.add(i);
-						sequenzaParziale.add(i);
-					}
-					else {
-						if(sommaUmidita > i.getSomma()) {
-							sommaUmidita = i.getSomma();
-							sequenzaParziale.set(livello-3, i);
-							sequenzaParziale.set(livello-2, i);
-							sequenzaParziale.set(livello-1, i);
-						}
-					}
-				}
-				sommaUmidita = 0;
-				livello++;
+			for(Citta c : mDao.getCitta()) {	
+				if(cittaParziale.size() < 15) {
+					cittaParziale.add(c);
+					cerca(mappaParziale, livello+1, mese, cittaParziale);
+					cittaParziale.remove(c);
+				}	
 			}
 		}
 	}
 
 	public Map<Citta, List<Rilevamento>> rilevamentiCitta(int mese) {
+		
 		for(Citta c : mDao.getCitta()) {
 			mappa.put(c, mDao.getAllRilevamentiLocalitaMese(mese, c.getNome()));
 		}
